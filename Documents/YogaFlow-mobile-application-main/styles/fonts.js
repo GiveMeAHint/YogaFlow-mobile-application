035c494d6075cd9432547729ea391a58cbd58ec5
catttcup <nastya_tvd@icloud.com>
2025-06-01 13:33:13 +0500

    Исправление: очистка отслеживаемых файлов

--- Documents/YogaFlow-mobile-application-main/styles/fonts.js
+++
@@ -1,6 +0,0 @@
-export const Fonts = {
-  HindVadodaraBold: 'HindVadodara_Bold',
-  HindVadodaraMedium: 'HindVadodara_Medium',
-  HindVadodaraRegular: 'HindVadodara_Regular',
-  HindVadodaraSemiBold: 'HindVadodara_SemiBold',
-};
